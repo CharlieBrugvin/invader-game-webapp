22f54f69f84b4a1d4aaea9edf993f2229d7a9065
CharlieBrugvin <charlie.brugvin@gmail.com>
2018-05-22 22:15:21 +0200

    control icons added

--- src/app/components/game/control-calc/control-calc.component.ts
+++ src/app/components/game/control-calc/control-calc.component.ts
@@ -4,42 +4,49 @@ import { Component, OnInit, EventEmitter, Output, ElementRef, HostListener } fro
   selector: 'app-control-calc',
   template:`
     <div class="left"
-        (mousedown)="downOnLeft.emit()"
-        (mouseup)  ="upOnLeft.emit()"
 
-        (touchstart)="downOnLeft.emit()"
-        (touchend)="upOnLeft.emit()"
-        
-        (keydown)="onKeyDown('left')"
-        >
-      <span>
-        LEFT
-      </span>
+        (mousedown)="onDownOnLeft()"
+        (mouseup)  ="onUpOnLeft()"
+
+        (touchstart)="onDownOnLeft()"
+        (touchend)="onUpOnLeft()">
+
+      <div class="left-btn" [ngClass]="{'pressed': leftPressed}">
+        <i class="fas fa-arrow-circle-left"></i>
+      </div>
+
     </div>
     
     <div class="middle"
-        (mousedown)="downOnMiddle.emit()"
-        (mouseup)  ="upOnMiddle.emit()"
-        (touchstart)="downOnMiddle.emit()"
-        (touchend)="upOnMiddle.emit()">
-        <span>
-          SHOOT !
-        </span>
+        (mousedown)="onDownOnMiddle()"
+        (mouseup)  ="onUpOnMiddle()"
+
+        (touchstart)="onDownOnMiddle()"
+        (touchend)="onUpOnMiddle()">
+
+        <div class="shoot-btn" [ngClass]="{'pressed': middlePressed}">
+          <i class="fas fa-crosshairs"></i>
+        </div>
+
     </div>
     
     <div class="right"
-        (mousedown)="downOnRight.emit()"
-        (mouseup)  ="upOnRight.emit()"
-        (touchstart)="downOnRight.emit()"
-        (touchend)="upOnRight.emit()">
-      <span>
-        RIGHT
-      </span>
+
+        (mousedown)="onDownOnRight()"
+        (mouseup)  ="onUpOnRight()"
+
+        (touchstart)="onDownOnRight()"
+        (touchend)="onUpOnRight()">
+
+      <div class="right-btn" [ngClass]="{'pressed': rightPressed}">
+        <i class="fas fa-arrow-circle-right"></i>
+      </div>
+
     </div>
   `,
   styleUrls: ['./control-calc.component.scss']
 })
-export class ControlCalcComponent implements OnInit {
+export class ControlCalcComponent  {
 
   @Output() downOnLeft: EventEmitter<null> = new EventEmitter();
   @Output() upOnLeft: EventEmitter<null> = new EventEmitter();
@@ -50,24 +57,33 @@ export class ControlCalcComponent implements OnInit {
   @Output() downOnRight: EventEmitter<null> = new EventEmitter();
   @Output() upOnRight: EventEmitter<null> = new EventEmitter();
 
+  rightPressed: boolean = false;
+  leftPressed: boolean = false;
+  middlePressed: boolean = false;
+  
+
   // we also emit if the user uses keys
   @HostListener('document:keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
     switch(event.code) { 
       case 'ArrowLeft': {
         this.downOnLeft.emit();
+        this.leftPressed = true;
         break; 
       } 
       case 'ArrowRight': { 
         this.downOnRight.emit();
+        this.rightPressed = true;
         break; 
       }
       case 'ArrowUp': {
         this.downOnMiddle.emit();
+        this.middlePressed = true;
         break; 
       } 
       case 'Space': {
         this.downOnMiddle.emit();
+        this.middlePressed = true;
         break; 
       } 
    } 
@@ -78,18 +94,22 @@ export class ControlCalcComponent implements OnInit {
     switch(event.code) { 
       case 'ArrowLeft': {
         this.upOnLeft.emit();
+        this.leftPressed = false;
         break; 
       } 
       case 'ArrowRight': { 
         this.upOnRight.emit();
+        this.rightPressed = false;
         break; 
       }
       case 'ArrowUp': {
         this.upOnMiddle.emit();
+        this.middlePressed = false;
         break; 
       } 
       case 'Space': {
         this.upOnMiddle.emit();
+        this.middlePressed = false;
         break; 
       } 
    }
@@ -98,6 +118,37 @@ export class ControlCalcComponent implements OnInit {
   constructor(private element: ElementRef) {
   }
 
-  ngOnInit() {
+  onDownOnLeft() {
+    this.downOnLeft.emit();
+    this.leftPressed = true;
+
   }
+
+  onUpOnLeft() {
+    this.upOnLeft.emit();
+    this.leftPressed = false;
+  }
+
+  onDownOnMiddle() {
+    this.downOnMiddle.emit();
+    this.middlePressed = true;
+  }
+
+  onUpOnMiddle() {
+    this.upOnMiddle.emit();
+    this.middlePressed = false;
+    
+  }
+
+  onDownOnRight() {
+    this.downOnRight.emit();
+    this.rightPressed = true;
+  }
+  
+  onUpOnRight() {
+    this.upOnRight.emit();
+    this.rightPressed = false;
+    
+  }
+
 }
